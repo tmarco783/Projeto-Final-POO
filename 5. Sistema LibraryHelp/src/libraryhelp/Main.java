f0181eb5fa645b8b169af95185c64494a0873b9f
Marco Túlio <tmarco783@gmail.com>
2017-12-02 20:07:15 -0200

    POO

--- 5. Sistema LibraryHelp/src/libraryhelp/Main.java
+++ 5. Sistema LibraryHelp/src/libraryhelp/Main.java
@@ -1,11 +1,12 @@
 package libraryhelp;
 import pessoas.*; /*Importando o pacote 'pessoas' e todas as suas classes*/
+import biblioteca.*; /*Importando o pacote 'biblioteca e todas as suas classes'*/
 public class Main {
     public static void main(String[] args) {
         
         /*Alguns testes de administrador na classe principal*/
        Funcionario func1 = new Funcionario(1598, "Aline", 25);
-       func1.cadastrarFuncionario(func1); 
+       func1.gravaDados(func1);
         
         
     }
